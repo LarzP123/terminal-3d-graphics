fd2e4eaa510de56da457dd9c5e4ca7b12755872a
Larckson <LarckLarck@gmail.com>
2025-12-27 20:41:19 -0600

    global epsilon

--- src/Tri.hs
+++ src/Tri.hs
@@ -4,14 +4,24 @@ import Vector
 import Matrix
 import Textures
 
--- | A Triangle made of 3 vertices of type 'a' in space, with a texture and a 2d mapping onto
+{-| This is a number that is barely above zero. This is so we can do 3d graphics by making sure every object is
+    This is so we can do 3d graphics by making sure every object is in front of you without dividing by zero. -}
+epsilon :: Double
+epsilon = 0.0001
+
+{-| A Triangle made of 3 space vertices of type 'a' in space, with a color mapping onto it.
+    The color mapping may either just be a solid color or a texture with 3 vec2s. Each space vertice maps to a 
+    vec2 UV texture mapping based on the order they are listed in. -}
 data Tri a = Tri a a a (ColorMapping Vec2) deriving Show
 
 instance Functor Tri where
     fmap :: (a -> b) -> Tri a -> Tri b
     fmap f (Tri a b c texMap) = Tri (f a) (f b) (f c) texMap
 
--- | Groups the space vertex with the corresponding UV vertex for mapping the texture
+{-| This extracts a list of space vertices and UV mapping vertices from a tri. This is so you can do
+    transformations on the space vertices and UV vertices at the same time by having them paired.
+    If you have a solid color mapping it will just return all of the UV vertices as 0 and create
+    a texture with 1 pixel of that color. -}
 triToVertGrouppings :: Tri a -> ([(a, Vec2)], [[RGB]])
 triToVertGrouppings (Tri a b c (Solid color)) = ([(a, 0), (b, 0), (c, 0)], [[color]])
 triToVertGrouppings (Tri v3a v3b v3c (Texture (TextureMapping tex v2a v2b v2c))) = ([(v3a, v2a), (v3b, v2b), (v3c, v2c)], tex)
@@ -19,7 +29,7 @@ triToVertGrouppings (Tri v3a v3b v3c (Texture (TextureMapping tex v2a v2b v2c)))
 -- | Turns space vertex and UV vertex mapping pairs and a texture int a Tri object
 vertGrouppingsToTri :: (a, Vec2) -> (a, Vec2) -> (a, Vec2) -> [[RGB]] -> Tri a
 -- If we have a bunch of zero vectors for the UV vectors, there is no real interpolation to be done and it will just be a solid color
-vertGrouppingsToTri (v3a, 0) (v3b, 0) (v3c, 0) tex = Tri v3a v3b v3c (Solid (head (head tex)))
+vertGrouppingsToTri (v3a, 0) (v3b, 0) (v3c, 0) tex = Tri v3a v3b v3c (Solid ((head . head) tex))
 -- If the texture is just one color, then we can just represent it as a type color and not need to do interpolation
 vertGrouppingsToTri (v3a, _) (v3b, _) (v3c, _) tex | length tex == 1 && length (head tex) == 1 =
     Tri v3a v3b v3c (Solid (head (head tex)))
@@ -82,7 +92,7 @@ interpolateUV bary (Texture (TextureMapping _ uvA uvB uvC)) =
 pointInsideTriColor :: Vec2 -> Tri Vec3 -> ColorMapping Vec2 -> Maybe (RGB, Double)
 pointInsideTriColor p tri colorMapping = do
     barycentricCoords <- barycentricDepth p tri
-    if not (insideTriangle (toVec3 barycentricCoords) && vL barycentricCoords > 0.01) then Nothing
+    if not (insideTriangle (toVec3 barycentricCoords) && vL barycentricCoords > epsilon) then Nothing
     else
         let rgb = case colorMapping of
                     Solid c -> c
@@ -117,19 +127,19 @@ clipBehindCamera = concatMap clipTri
                 ([a,b,c], _) -> [vertGrouppingsToTri a b c tex]  -- fully in front
                 _ -> [] -- Fully Behind
         -- testing if the 3d space point is in front of you. 2d space points don't have depth so discarded
-        inFront (vec3, _) = vL vec3 > 0.01
+        inFront (vec3, _) = vL vec3 > epsilon
 
 -- | Interpolate between two vertices at z=0
 lerpVertGroupping :: (Vec3, Vec2) -> (Vec3, Vec2) -> (Vec3, Vec2)
 lerpVertGroupping (vec3Front, vec2Front) (vec3Back, vec2Back) =
     let t = if vL vec3Front == vL vec3Back
-            then 0.5  -- midpoint as fallback
+            then 0.5 -- midpoint as fallback
             else vL vec3Front / (vL vec3Front - vL vec3Back)
     in (Vec3
-        (vF vec3Front + t * (vF vec3Back - vF vec3Front))
-        (vM vec3Front + t * (vM vec3Back - vM vec3Front))
-        0.01,
+            (vF vec3Front + t * (vF vec3Back - vF vec3Front))
+            (vM vec3Front + t * (vM vec3Back - vM vec3Front))
+            epsilon,
         Vec2
-        (vF vec2Front + t * (vF vec2Back - vF vec2Front))
-        (vL vec2Front + t * (vL vec2Back - vL vec2Front))
+            (vF vec2Front + t * (vF vec2Back - vF vec2Front))
+            (vL vec2Front + t * (vL vec2Back - vL vec2Front))
     )
