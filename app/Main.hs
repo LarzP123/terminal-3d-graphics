19a5fe0fe5f3136a81e41970c8705eaa8105357f
Larz Piechocki <larzpiechocki@gmail.com>
2026-06-29 17:27:46 -0500

    Add Chess World

--- app/Main.hs
+++ app/Main.hs
@@ -8,20 +8,36 @@ portalWorld :: IO [Tri Vec3]
 portalWorld = do
     [cubeTexture, wallTexture, floorTexture, portalOrangeTexture, portalBlueTexture]
         <- mapM readBMP [ "textures/cube.bmp", "textures/wall.bmp", "textures/floor.bmp", "textures/portalOrange.bmp", "textures/portalBlue.bmp" ]
-    let cube = (fmap . fmap) (+ Vec3 20 (-15) 15) (cubeFormer cubeTexture)
-        room = roomFormer (Vec3 (-30) (-35) (-60)) (Vec3 70 35 65) floorTexture wallTexture
+    let cube = (fmap . fmap) (+ Vec3 20 (-15) 15) (cubeFormer (texWallFormer cubeTexture))
+        room = roomFormer (Vec3 (-30) (-35) (-60)) (Vec3 70 35 65) (texWallFormer floorTexture) (texWallFormer wallTexture)
         portalMin = Vec3 (-28) (-25) (-25)
         portalMax = Vec3   68    20    25
-        portal = portalFormer portalBlueTexture portalOrangeTexture
+        portal = portalFormer (texWallFormer portalBlueTexture) (texWallFormer portalOrangeTexture)
             ( comp3Reduce portalMin portalMin portalMax, comp3Reduce portalMin portalMax portalMin )
             ( comp3Reduce portalMax portalMin portalMax, comp3Reduce portalMax portalMax portalMin )
             False
         lights = [Ray (Vec3 0.25 0.25 0.25), Ambient 0.5]
     pure (bakeLight lights <$> concat [cube, room, portal])
 
+-- | A world with a portal on the side of each wall
+chessWorld :: IO [Tri Vec3]
+chessWorld = do
+    let tileTris i j =
+            let fi = fromIntegral i
+                fj = fromIntegral j
+                (v0, v1, v2, v3) = ( Vec3 fi 0 fj, Vec3 (fi + 1) 0 fj, Vec3 (fi + 1) 0 (fj + 1), Vec3 fi 0 (fj + 1) )
+                col = if even (i + j) then Solid RGB { red = 250, green = 250, blue = 250 }
+                    else Solid RGB { red = 0, green = 0, blue = 0 }
+            in [ Tri v0 v1 v2 col, Tri v0 v2 v3 col ]
+        board = concat [ tileTris i j | i <- [0 :: Int .. 8], j <- [0 :: Int .. 8] ]
+        movedBoard = (fmap . fmap) ((+ Vec3 (-5) (-10) (-5)) . (* 5)) board
+        skyBlue = RGB { red = 0, green = 0, blue = 250 }
+        room = roomFormer (Vec3 (-100) (-100) (-100)) (Vec3 100 100 100) (solWallFormer skyBlue) (solWallFormer skyBlue)
+    pure ((bakeLight [Ambient 0.5] <$> movedBoard) ++ room)
+
 -- | A list of all of the demo worlds to show off, and a corresponding name
 demoWorlds :: [(String, IO [Tri Vec3])]
-demoWorlds = [ ("portal", portalWorld)]
+demoWorlds = [ ("portal", portalWorld), ("chess", chessWorld)]
 
 -- | A function that prompts the user with a list of demo worlds and has them enter a number to choose one. It then returns that demo world
 chooseDemoWorld :: IO [Tri Vec3]
--- src/Terminal3D/Objects.hs
+++ src/Terminal3D/Objects.hs
@@ -6,51 +6,56 @@ import Terminal3D.Textures
 import Terminal3D.Matrix
 
 -- | Build a textured quad (two triangles) from four corner vertices and a texture
-wallFormer :: Texture -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> [Tri Vec3]
-wallFormer tex v0 v1 v2 v3 =
+texWallFormer :: Texture -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> [Tri Vec3]
+texWallFormer tex v0 v1 v2 v3 =
     [ Tri v0 v1 v2 (Texture (TextureMapping tex (Vec2 0 0) (Vec2 1 0) (Vec2 1 1)))
     , Tri v0 v2 v3 (Texture (TextureMapping tex (Vec2 0 0) (Vec2 1 1) (Vec2 0 1)))
     ]
 
+-- | Build a textured quad (two triangles) from four corner vertices and a solid
+solWallFormer :: RGB -> Vec3 -> Vec3 -> Vec3 -> Vec3 -> [Tri Vec3]
+solWallFormer rgb v0 v1 v2 v3 =
+    [ Tri v0 v1 v2 (Solid rgb)
+    , Tri v0 v2 v3 (Solid rgb)
+    ]
+
+type WallFormer = Vec3 -> Vec3 -> Vec3 -> Vec3 -> [Tri Vec3]
+
 -- | Builds a room from a Vec3 featuring one corner and another with the opposite corner
-roomFormer :: Vec3 -> Vec3 -> Texture -> Texture -> [Tri Vec3]
-roomFormer roomMin roomMax floorTexture wallTexture =
+roomFormer :: Vec3 -> Vec3 -> WallFormer -> WallFormer -> [Tri Vec3]
+roomFormer roomMin roomMax floorFormer wallFormer =
     let 
-        roomFloor = fmap flipTri (wallFormer floorTexture
-                        (comp3Reduce roomMin roomMin roomMin)
-                        (comp3Reduce roomMax roomMin roomMin)
-                        (comp3Reduce roomMax roomMin roomMax)
-                        (comp3Reduce roomMin roomMin roomMax))
-        wallFront = wallFormer wallTexture
-                        (comp3Reduce roomMin roomMin roomMax)
-                        (comp3Reduce roomMax roomMin roomMax)
-                        (comp3Reduce roomMax roomMax roomMax)
-                        (comp3Reduce roomMin roomMax roomMax)
-        wallBack  = wallFormer wallTexture
-                        (comp3Reduce roomMax roomMin roomMin)
-                        (comp3Reduce roomMin roomMin roomMin)
-                        (comp3Reduce roomMin roomMax roomMin)
-                        (comp3Reduce roomMax roomMax roomMin)
-        wallLeft  = wallFormer wallTexture
-                        (comp3Reduce roomMin roomMin roomMin)
-                        (comp3Reduce roomMin roomMin roomMax)
-                        (comp3Reduce roomMin roomMax roomMax)
-                        (comp3Reduce roomMin roomMax roomMin)
-        wallRight = wallFormer wallTexture
-                        (comp3Reduce roomMax roomMin roomMax)
-                        (comp3Reduce roomMax roomMin roomMin)
-                        (comp3Reduce roomMax roomMax roomMin)
-                        (comp3Reduce roomMax roomMax roomMax)
+        roomFloor = fmap flipTri (floorFormer
+            (comp3Reduce roomMin roomMin roomMin)
+            (comp3Reduce roomMax roomMin roomMin)
+            (comp3Reduce roomMax roomMin roomMax)
+            (comp3Reduce roomMin roomMin roomMax))
+        wallFront = wallFormer
+            (comp3Reduce roomMin roomMin roomMax)
+            (comp3Reduce roomMax roomMin roomMax)
+            (comp3Reduce roomMax roomMax roomMax)
+            (comp3Reduce roomMin roomMax roomMax)
+        wallBack  = wallFormer
+            (comp3Reduce roomMax roomMin roomMin)
+            (comp3Reduce roomMin roomMin roomMin)
+            (comp3Reduce roomMin roomMax roomMin)
+            (comp3Reduce roomMax roomMax roomMin)
+        wallLeft  = wallFormer
+            (comp3Reduce roomMin roomMin roomMin)
+            (comp3Reduce roomMin roomMin roomMax)
+            (comp3Reduce roomMin roomMax roomMax)
+            (comp3Reduce roomMin roomMax roomMin)
+        wallRight = wallFormer
+            (comp3Reduce roomMax roomMin roomMax)
+            (comp3Reduce roomMax roomMin roomMin)
+            (comp3Reduce roomMax roomMax roomMin)
+            (comp3Reduce roomMax roomMax roomMax)
     in concat [roomFloor, wallFront, wallBack, wallLeft, wallRight]
 
 
 -- | Build a pair of linked portal quads with decorative borders
-portalFormer
-    :: Texture -> Texture
-    -> (Vec3, Vec3) -> (Vec3, Vec3)
-    -> Bool
-    -> [Tri Vec3]
-portalFormer borderTexA borderTexB (vA0, vA2) (vB0, vB2) flipPortal =
+portalFormer :: WallFormer -> WallFormer -> (Vec3, Vec3) -> (Vec3, Vec3) -> Bool -> [Tri Vec3]
+portalFormer texAFormer texBFormer (vA0, vA2) (vB0, vB2) flipPortal =
     let vA1 = Vec3 (vF vA0) (vM vA2) (vL vA0)
         vA3 = Vec3 (vF vA2) (vM vA0) (vL vA2)
         vB1 = Vec3 (vF vB0) (vM vB2) (vL vB0)
@@ -63,12 +68,12 @@ portalFormer borderTexA borderTexB (vA0, vA2) (vB0, vB2) flipPortal =
        , Tri vB0 vB1 vB2 (Portal vA0 vA1 vA2 portalRotMatrix)
        , Tri vB0 vB2 vB3 (Portal vA0 vA2 vA3 portalRotMatrix)
        ]
-       ++ borderFormer borderTexA (vA0, vA1, vA2, vA3) True
-       ++ borderFormer borderTexB (vB0, vB1, vB2, vB3) False
+       ++ borderFormer texAFormer (vA0, vA1, vA2, vA3) True
+       ++ borderFormer texBFormer (vB0, vB1, vB2, vB3) False
 
 -- | Build a slightly-scaled border quad around a portal face
-borderFormer :: Texture -> (Vec3, Vec3, Vec3, Vec3) -> Bool -> [Tri Vec3]
-borderFormer tex (v0, v1, v2, v3) flipBool =
+borderFormer :: WallFormer -> (Vec3, Vec3, Vec3, Vec3) -> Bool -> [Tri Vec3]
+borderFormer wallFormer (v0, v1, v2, v3) flipBool =
     let normNotDirec = (v3 - v0) `cross` (v2 - v0)
         direc        = if flipBool then negate normNotDirec else normNotDirec
         borderOffset = vMap (*0.01) (signum direc)
@@ -78,20 +83,20 @@ borderFormer tex (v0, v1, v2, v3) flipBool =
         b1 = vMap scalOp (v1 - center) + center + borderOffset
         b2 = vMap scalOp (v2 - center) + center + borderOffset
         b3 = vMap scalOp (v3 - center) + center + borderOffset
-    in wallFormer tex b0 b1 b2 b3
+    in wallFormer b0 b1 b2 b3
 
 -- | Build a textured unit cube centred at the origin (side length 20)
-cubeFormer :: Texture -> [Tri Vec3]
-cubeFormer tex =
+cubeFormer :: WallFormer -> [Tri Vec3]
+cubeFormer sideFormer =
     let p000 = Vec3 (-10) (-10) (-10); p001 = Vec3 (-10) (-10) 10
         p010 = Vec3 (-10)  10  (-10);  p011 = Vec3 (-10)  10   10
         p100 = Vec3  10  (-10) (-10);  p101 = Vec3  10  (-10)  10
         p110 = Vec3  10   10  (-10);   p111 = Vec3  10   10    10
     in concat
-        [ wallFormer tex p001 p101 p111 p011           -- Front
-        , fmap flipTri (wallFormer tex p100 p000 p010 p110) -- Back
-        , wallFormer tex p000 p001 p011 p010           -- Left
-        , wallFormer tex p101 p100 p110 p111           -- Right
-        , wallFormer tex p011 p111 p110 p010           -- Top
-        , wallFormer tex p000 p100 p101 p001           -- Bottom
+        [ sideFormer p001 p101 p111 p011           -- Front
+        , fmap flipTri (sideFormer p100 p000 p010 p110) -- Back
+        , sideFormer p000 p001 p011 p010           -- Left
+        , sideFormer p101 p100 p110 p111           -- Right
+        , sideFormer p011 p111 p110 p010           -- Top
+        , sideFormer p000 p100 p101 p001           -- Bottom
         ]
